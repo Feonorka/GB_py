de1e72da456aa2d152f8d0ea6b68c7613bbd28cf
Feonorka <104940479+Feonorka@users.noreply.github.com>
2022-12-02 23:49:30 +0300

    Сделана вторая домашка

--- Exp_2/HomeWork_3.py
+++ Exp_2/HomeWork_3.py
@@ -1,7 +1,7 @@
 # Вводим с клавиатуры целое число - это зарплата.
 # Нужно вывести в консоль -  Минимальное кол-во  купюр, которыми можно выдать ЗП.
 # И сколько, и каких бухгалтер выдаст 25 рублевых купюр,  10 рублевых, 3 рублевых и 1 рублевых
-kassa_dict = {25: 4, 10: 10, 3: 30, 1: 100}
+kassa_dict = {25: 40, 10: 100, 3: 300, 1: 1000}
 polychka = 0
 
 def inventory(data):
@@ -9,15 +9,29 @@ def inventory(data):
   lis = [key* val for key, val in data.items()]
   return lis
 
-def zp_money(dict_of_limit, sum):
+def zp_money(dict_of_limit, sum_zp):
     money_dict = dict()
+    temp_sum = 0
     
+    for key in dict_of_limit:
+        temp_lim = int(dict_of_limit[key])
+        count = 0
+        while temp_sum <= sum_zp:
+            if count <= temp_lim:
+                if temp_sum + key <= sum_zp:
+                    temp_sum += key
+                    count += 1
+                    money_dict[key] = count
+                else:
+                    break
+            else:
+                break
     return money_dict
 
 kassa_limit_money = inventory(kassa_dict)
 money_limit = sum(kassa_limit_money)
 print(money_limit)
-
+print(kassa_limit_money)
 zp = int(input('Введите число: '))
 
 if zp > money_limit:
@@ -25,26 +39,3 @@ if zp > money_limit:
     exit
 else:
     print(zp_money(kassa_dict, zp))
-
-# salary = int(input('Write salary: '))
-# salary_help = salary
-# bills = [0, 0, 0, 0]
-# F = []
-# F.append(0)
-# bills_count = 4
-# bills_nominal = [25, 10, 3, 1]
-# n = salary
-# for i in range(1, n + 1, 1):
-#     F.append(100000)
-#     for j in range(bills_count):
-#         if i >= bills_nominal[j] and F[i - bills_nominal[j]] + 1 < F[i]:
-#             F[i] = F[i - bills_nominal[j]] + 1
-
-# while n > 0:
-#     for i in range(bills_count):
-#         if F[n - bills_nominal[i]] == F[n] - 1:
-#             bills[i] += 1
-#             n -= bills_nominal[i]
-#             break
-
-# print(f'Salary {salary} can be paid as: 25: {bills[0]}, 10: {bills[1]}, 3: {bills[2]}, 1: {bills[3]}')
