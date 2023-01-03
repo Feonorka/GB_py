f0795697dadda5e77a50ca4328ff3ae2074fd302
Feonorka <104940479+Feonorka@users.noreply.github.com>
2023-01-03 14:40:49 +0300

    Задачки с классами

--- Exp_8/HomeWork_2.py
+++ Exp_8/HomeWork_2.py
@@ -2,3 +2,24 @@
 # Где класс будет содержать имя студента name, номер группы group и список полученных оценок progress.
 # В самой программе вводим список студентов.
 # После выводим список , отсортированный по имени, потом выводятся студенты, имеющие неудовлетворительные оценки.
+class Student_name:
+    name = 'no'
+    def init(self, n):
+        self.name = str(n)
+class Group:
+    group = 'no'
+    def init(self, g):
+        self.group = str(g)
+class Point:
+    poin = 0
+    def init(self, p):
+        self.point = int(p)
+
+n1 = Student_name(input().split(" "))
+g1 = Group(['CX - 11', 'YX - 22', 'BV - 33'])
+p1 = Point(int(input()))
+point_dict = {}
+for i in n1.name:
+    point_dict[i] = p1.point
+
+print(point_dict)
